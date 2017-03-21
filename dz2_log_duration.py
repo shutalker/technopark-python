b4cb59958182c49fb0a0e000794414ed8be443e4
Alexey Shatunov <shatunov.alexey@mail.ru>
2017-03-21 21:42:18 +0300

    Задание 1 из второго дз. Простой декоратор, записывающий в лог-файл время выполнения декорируемой функции

---
+++ dz2_log_duration.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+
+
+def log_duration(func):
+
+    '''It's a decorator that prints the time of executing other functions'''
+
+    def decorated(*args, **kwargs):
+        with open('function_execution_log.txt', 'w') as wf:
+            log_string = 'File: ' + __file__ + '\n'
+            log_string += 'Invoking function: ' + func.__name__ + '\n'
+            log_string += 'Time of invoking: ' + datetime.now().__str__()
+            wf.write(log_string)
+        func(*args, **kwargs)
+
+    return decorated
+
+
+@log_duration
+def test_function(name):
+
+    '''This function is just for testing decorator'''
+
+    print('Hello, {}!'.format(name))
+
+
+if __name__ == '__main__':
+    first_name = input('Enter your name here: ')
+    test_function(first_name)
